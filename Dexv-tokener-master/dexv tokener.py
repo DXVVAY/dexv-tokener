33118f98b903251cb996bcb08648e3c7102aa435
DEXV <89728480+DXVVAY@users.noreply.github.com>
2022-08-11 18:22:01 +0200

    Update dexv tokener.py

--- Dexv-tokener-master/dexv tokener.py
+++ Dexv-tokener-master/dexv tokener.py
@@ -26,36 +26,6 @@ def randstr(lenn):
 
 
 
-
-def friender(token, user):
-    try:
-        user = user.split("#")
-        headers = {
-            "accept": "*/*",
-            "accept-encoding": "gzip, deflate, br",
-            "accept-language": "en-GB",
-            "authorization": token,
-            "content-length": "90",
-            "content-type": "application/json",
-            "cookie": f"__cfuid={randstr(43)}; __dcfduid={randstr(32)}; locale=en-US",
-            "origin": "https://discord.com",
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/1.0.9003 Chrome/91.0.4472.164 Electron/13.4.0 Safari/537.36",
-            "x-debug-options": "bugReporterEnabled",
-            "x-super-properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDAzIiwib3NfdmVyc2lvbiI6IjEwLjAuMjI0NjMiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6InNrIiwiY2xpZW50X2J1aWxkX251bWJlciI6OTkwMTYsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGx9"
-        }
-        payload = {"username": user[0], "discriminator": user[1]}
-        src = requests.post('https://canary.discordapp.com/api/v6/users/@me/relationships', headers=headers,
-                            json=payload)
-        if src.status_code == 204:
-            print(f"[+] Friend request sent to {user[0]}#{user[1]}! [{token}]")
-    except Exception as e:
-        print(e)
-
-
-
 def spammer():
     print("██████╗░███████╗██╗░░██╗██╗░░░██╗  ████████╗░█████╗░██╗░░██╗███████╗███╗░░██╗███████╗██████╗░")
     print("██╔══██╗██╔════╝╚██╗██╔╝██║░░░██║  ╚══██╔══╝██╔══██╗██║░██╔╝██╔════╝████╗░██║██╔════╝██╔══██╗")
